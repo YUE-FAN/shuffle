2c1d16d66d3db5fe4f3f20f7653795bc183c6621
Yue Fan <yfan@srv-12-71.mpi-inf.mpg.de>
2019-06-18 17:36:08 +0200

    modify CAM

--- CAM.py
+++ CAM.py
@@ -40,12 +40,12 @@ def main(args):
 
     if args.dataset == 'cifar10':
         num_class = 10
-        valset = datasets.CIFAR10(root=args.dat, train=False, download=False, transform=transform_test)
+        valset = datasets.CIFAR10(root=args.data, train=False, download=False, transform=transform_test)
         val_loader = torch.utils.data.DataLoader(valset, batch_size=args.test_batch, shuffle=False,
                                                  num_workers=args.workers)
     elif args.dataset == 'cifar100':
         num_class = 100
-        valset = datasets.CIFAR100(root=args.dat, train=False, download=False, transform=transform_test)
+        valset = datasets.CIFAR100(root=args.data, train=False, download=False, transform=transform_test)
         val_loader = torch.utils.data.DataLoader(valset, batch_size=args.test_batch, shuffle=False,
                                                  num_workers=args.workers)
     else:
@@ -60,6 +60,13 @@ def main(args):
             layer=args.layer
         )
     elif args.arch.endswith('vgg16'):
+        model = models.__dict__[args.arch](
+            num_classes=num_class,
+            include_top=False,
+            dropout_rate=0,
+            layer=args.layer
+        )
+    elif args.arch.endswith('d1_resnet50'):
         model = models.__dict__[args.arch](
             num_classes=num_class,
             include_top=False,
@@ -98,6 +105,7 @@ def test(val_loader, model, criterion, epoch, use_cuda):
     model.eval()
     y_true = []
     y_pred = []
+    avgpool = nn.AdaptiveAvgPool2d(1)
     with torch.no_grad():
         end = time.time()
         bar = Bar('Processing', max=len(val_loader))
@@ -112,19 +120,22 @@ def test(val_loader, model, criterion, epoch, use_cuda):
             # compute output
             model.include_top = False
             outputs = model(inputs)  # [bs, num_feature, h, w]
-            weights = model.fc.weight  # [num_class, num_feature]
+            weights = model.module.fc.weight.data.cuda()  # [num_class, num_feature]
             num_class, num_feature = weights.size()
 
             # [bs, num_class, h, w]
             cam = torch.empty(outputs.size(0), num_class, outputs.size(2), outputs.size(3)).cuda()
             for i in range(num_class):
-                cam[:, i, :, :] = torch.matmul(outputs, weights[i].view(1, num_feature, 1, 1)).sum(dim=1)
-            cam = cam[:, targets, :, :]  # select the ones corresponds to the correct label
+                tmp = outputs * weights[i].view(1, num_feature, 1, 1)  # [bs, num_feature, h, w]
+                cam[:, i, :, :] = tmp.sum(dim=1)
 
+
+            cam = cam[torch.arange(cam.size(0)), targets, :, :]  # select the ones corresponds to the correct label
 
             # select the ones which get predicted correctly
-            model.include_top = True
-            outputs = model(inputs)  # [bs, num_feature, h, w]
+            outputs = avgpool(outputs).view(cam.size(0), -1)
+            outputs = torch.matmul(outputs, weights.t())
+
             _, pred = outputs.data.topk(1, 1, True, True)
             pred = pred.view(-1, )
             idx = pred.eq(targets)
@@ -133,11 +144,16 @@ def test(val_loader, model, criterion, epoch, use_cuda):
             inputs = inputs[idx!=0]
 
             draw_CAM(cam, inputs)
-
+            break
             bar.next()
         bar.finish()
-
     return
+
+
+def linear_mapping(x):
+    xmin = np.min(x)
+    xmax = np.max(x)
+    return 255/(xmax-xmin)*(x-xmin)
 
 def draw_CAM(cams, inputs):
     """
@@ -149,16 +165,36 @@ def draw_CAM(cams, inputs):
     bs, _, h, w = inputs.size()
     for i in range(bs):
         cam = cams[i]
-        img = inputs[i].transpose(1, 2, 0)
+        img = inputs[i].permute(1, 2, 0)
+        img *= torch.tensor([0.2023, 0.1994, 0.2010]).view(1, 1, 3).cuda()
+        img += torch.tensor([0.4914, 0.4822, 0.4465]).view(1, 1, 3).cuda()
+        img *= 255
+        img = img.cpu().numpy()
+        img = np.uint8(img)
+
+        cam = cam.cpu().data.numpy()
+        cam = cv2.resize(cam, (32, 32), interpolation=cv2.INTER_CUBIC)
+        cam = np.clip(cam, np.mean(cam), np.max(cam))
+        cam = linear_mapping(cam)
+
+        # cmap = matplotlib.cm.jet
+        # plt.imshow(cam, cmap=cmap)
+        # plt.show()
+
+        cam = cv2.applyColorMap(np.uint8(cam), cv2.COLORMAP_JET)
+        img_save = cv2.addWeighted(cam, 0.3, img, 0.7, 0)
+        cv2.imwrite(str(i)+".png", img_save)
 
 
+        # break
+        # from PIL import Image
+        # i = Image.fromarray(np.uint8(img))
+        # i.show()
+        # break
 
-        cam = cv2.resize(cam, (64, 128), interpolation=cv2.INTER_CUBIC)
-        cmap = matplotlib.cm.jet
-        plt.imshow(cam, cmap=cmap)
-        # plt.savefig(all_dir+'/{}/sa.jpg'.format(id))
-        plt.savefig('./{}.jpg'.format(i))
 
+        # # plt.savefig(all_dir+'/{}/sa.jpg'.format(id))
+        # plt.savefig('./{}.jpg'.format(i))
 
 
 class Employee:
@@ -167,10 +203,10 @@ class Employee:
 
 if __name__ == '__main__':
     args = Employee()  # Create an empty emp
-    args.dataset = 'cifar10'
-    args.arch = 'resnet50'
-    # args.data = '/BS/database11/mini-imagenet64/'
-    args.data = '/data/users/yuefan/fanyue/dconv/data/'
+    args.dataset = 'cifar100'
+    args.arch = 'd1_resnet50'
+    args.data = '/BS/databases00/cifar-100'
+    # args.data = '/data/users/yuefan/fanyue/dconv/data/'
     args.img_size = 32
     args.gpu_id = '0'
     args.workers = 0
@@ -193,18 +229,23 @@ if __name__ == '__main__':
 
     # layers = [11, 12, 21, 22, 31, 32, 33, 41, 42, 43, 51, 52, 53, 99]
     layers = [00, 10, 11, 12, 20, 21, 22, 23, 30, 31, 32, 33, 34, 35, 40, 41, 41, 99]
-    m = []
-    for i in layers:
-        args.layer = i
-        if i == 0:
-            args.resume = '/BS/yfan/work/trained-models/dconv/checkpoints/mini-imagenet/resnet501d_300_imgsize64_noDA/resnet501d_0042_300/model_best.pth.tar'
-        else:
-            args.resume = '/BS/yfan/work/trained-models/dconv/checkpoints/mini-imagenet/resnet501d_300_imgsize64_noDA/resnet501d_' + str(
-                i) + '42_300/model_best.pth.tar'
-        print(args.resume)
-        m.append(main(args))
-    print(m)
-    import pickle
+    args.layer = 99
+    args.resume = '/BS/yfan/work/trained-models/dconv/checkpoints/cifar100/resnet501d_300_offlineDA/resnet501d_9942_300/model_best.pth.tar'
+    main(args)
 
-    with open("resnet501d_mini_imgsize64_noDA.txt", "wb") as fp:  # Pickling
-        pickle.dump(m, fp)
+
+    # m = []
+    # for i in layers:
+    #     args.layer = i
+    #     if i == 0:
+    #         args.resume = '/BS/yfan/work/trained-models/dconv/checkpoints/mini-imagenet/resnet501d_300_imgsize64_noDA/resnet501d_0042_300/model_best.pth.tar'
+    #     else:
+    #         args.resume = '/BS/yfan/work/trained-models/dconv/checkpoints/mini-imagenet/resnet501d_300_imgsize64_noDA/resnet501d_' + str(
+    #             i) + '42_300/model_best.pth.tar'
+    #     print(args.resume)
+    #     m.append(main(args))
+    # print(m)
+    # import pickle
+    #
+    # with open("resnet501d_mini_imgsize64_noDA.txt", "wb") as fp:  # Pickling
+    #     pickle.dump(m, fp)
