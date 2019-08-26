c290f729bb1f0b85aa0ed2bf5ae70effa27ec330
Yue Fan <yfan@srv-12-71.mpi-inf.mpg.de>
2019-08-26 19:29:12 +0200

    add 3 tool-scripts

---
+++ OtherBranch/finetune_vgg16_small.py
@@ -0,0 +1,374 @@
+'''
+Take the ref, train a VGG16_4353
+
+The first 2 layers are frozen, lr is 10 times smaller than training from scratch,
+last 4 conv layers and FCs are initialized as if it were from scratch.
+
+'''
+from __future__ import print_function
+
+import argparse
+import os
+import shutil
+import time
+import random
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import models.cifar as models
+from offlineDA import SmallImageNet
+from models.cifar import VGG16_1x1
+from models.cifar.vggfy import CONV_1x1
+
+from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+
+# ['alexnet', 'bottleneck', 'conv_1_7x7', 'densenet', 'identity_block3', 'preresnet', 'resnet', 'resnext', 'vgg11',
+# 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', 'wrn']
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
+# Datasets
+parser.add_argument('-d', '--data', default='path to dataset', type=str)
+parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+# Optimization options
+parser.add_argument('--epochs', default=300, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--train-batch', default=128, type=int, metavar='N',
+                    help='train batchsize')
+parser.add_argument('--test-batch', default=100, type=int, metavar='N',
+                    help='test batchsize')
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--drop', '--dropout', default=0, type=float,
+                    metavar='Dropout', help='Dropout ratio')
+parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
+                        help='Decrease learning rate at these epochs.')
+parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
+                    metavar='W', help='weight decay (default: 5e-4 for cifar dataset)')
+# Checkpoints
+parser.add_argument('-c', '--checkpoint', default='checkpoint', type=str, metavar='PATH',
+                    help='path to save checkpoint (default: checkpoint)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+# Architecture
+parser.add_argument('--img_size', type=int)
+parser.add_argument('--DA', action='store_true')
+# Miscs
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+#Device options
+parser.add_argument('--gpu-id', default='0', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')  # 0,1
+
+args = parser.parse_args()
+state = {k: v for k, v in args._get_kwargs()}
+
+# Use CUDA
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+use_cuda = torch.cuda.is_available()
+
+# Random seed
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+random.seed(args.manualSeed)
+np.random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+if use_cuda:
+    torch.cuda.manual_seed_all(args.manualSeed)
+    torch.backends.cudnn.deterministic = True
+best_acc = 0  # best test accuracy
+
+
+def main():
+    global best_acc
+    start_epoch = args.start_epoch  # start from epoch 0 or last checkpoint epoch
+
+    if not os.path.isdir(args.checkpoint):
+        mkdir_p(args.checkpoint)
+
+    # Data loading code
+    print('==> Preparing dataset')
+    if args.DA:
+        print('use DA')
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),  # with p = 1
+            transforms.RandomHorizontalFlip(),  # with p = 0.5
+            transforms.ToTensor(),  # it must be this guy that makes it CHW again
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+    else:
+        print('no DA')
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ])
+
+    if args.img_size == 32:
+        trainset = SmallImageNet(args.data, args.img_size, True, transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True,
+                                                   num_workers=args.workers)
+
+        valset = SmallImageNet(args.data, args.img_size, False, transform=transform_test)
+        val_loader = torch.utils.data.DataLoader(valset, batch_size=args.test_batch, shuffle=False,
+                                                 num_workers=args.workers)
+    elif args.img_size == 64:
+        if args.DA:
+            raise Exception('DA is not currenly supported by imgsize 64!!!')
+
+        trainset = SmallImageNet(args.data, args.img_size, True, transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True,
+                                                   num_workers=args.workers)
+
+        valset = SmallImageNet(args.data, args.img_size, False, transform=transform_test)
+        val_loader = torch.utils.data.DataLoader(valset, batch_size=args.test_batch, shuffle=False,
+                                                 num_workers=args.workers)
+    else:
+        raise Exception('img size can only be 32 or 64')
+
+    # create model
+    model = VGG16_1x1(0, 1000, True, 99)
+    print('loading pre-trained weights')
+    pp = '/BS/yfan/work/trained-models/dconv/checkpoints/small-imagenet/vgg161x1_90_imgsize32_DA/vgg161x1_9953_90/checkpoint.pth.tar'
+    checkpoint = torch.load(pp)
+    model.load_state_dict(checkpoint['state_dict'], False)
+    model.conv43 = nn.Sequential(CONV_1x1(512, 512, stride=1, padding=0, bias=True),
+                                 nn.MaxPool2d(kernel_size=2, stride=2))
+    model.conv51 = CONV_1x1(512, 512, stride=1, padding=0, bias=True)
+    model.conv52 = CONV_1x1(512, 512, stride=1, padding=0, bias=True)
+    model.conv53 = nn.Sequential(CONV_1x1(512, 512, stride=1, padding=0, bias=True),
+                                 nn.MaxPool2d(kernel_size=2, stride=2))
+    model.fc = nn.Sequential(nn.Linear(512, 4096),
+                             nn.ReLU(True),
+                             nn.Linear(4096, 4096),
+                             nn.ReLU(True),
+                             nn.Linear(4096, 1000))
+
+    for name, module in model.named_children():
+        if name in ['conv11', 'conv12']:
+            for p in module.parameters():
+                p.require_grads = False
+        if name in ['conv43', 'conv51', 'conv52', 'conv53']:
+            for m in module.modules():
+                if isinstance(m, nn.Conv2d):
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+    print('Total params: %.2fM' % (sum(p.numel() for p in filter(lambda p: p.requires_grad, model.parameters())) / 1000000.0))
+
+    model = torch.nn.DataParallel(model).cuda()
+
+    cudnn.benchmark = False
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    # Resume
+    title = 'small-ImageNet'
+    if args.resume:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isfile(args.resume), 'Error: no checkpoint directory found!'
+        args.checkpoint = os.path.dirname(args.resume)
+        checkpoint = torch.load(args.resume)
+        best_acc = checkpoint['best_acc']
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title, resume=True)
+        for param_group in optimizer.param_groups:
+            state['lr'] = param_group['lr']
+    else:
+        logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title)
+        logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.'])
+
+    if args.evaluate:
+        print('\nEvaluation only')
+        test_loss, test_acc = test(val_loader, model, criterion, start_epoch, use_cuda)
+        print(' Test Loss:  %.8f, Test Acc:  %.2f' % (test_loss, test_acc))
+        return
+
+    # Train and val
+    for epoch in range(start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch)
+
+        print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, args.epochs, state['lr']))
+
+        train_loss, train_acc = train(train_loader, model, criterion, optimizer, epoch, use_cuda)
+        test_loss, test_acc = test(val_loader, model, criterion, epoch, use_cuda)
+
+        # append logger file
+        logger.append([state['lr'], train_loss, test_loss, train_acc, test_acc])
+
+        # save model
+        is_best = test_acc > best_acc
+        best_acc = max(test_acc, best_acc)
+        save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'acc': test_acc,
+                'best_acc': best_acc,
+                'optimizer': optimizer.state_dict(),
+            }, is_best, checkpoint=args.checkpoint)
+
+    logger.close()
+    logger.plot()
+    savefig(os.path.join(args.checkpoint, 'log.eps'))
+
+    print('Best acc:')
+    print(best_acc)
+
+
+def train(train_loader, model, criterion, optimizer, epoch, use_cuda):
+    # switch to train mode
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    end = time.time()
+
+    bar = Bar('Processing', max=len(train_loader))
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda(async=True)
+        inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
+
+        # compute output
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+        losses.update(loss.data.item(), inputs.size(0))
+        top1.update(prec1.item(), inputs.size(0))
+        top5.update(prec5.item(), inputs.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # plot progress
+        bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                    batch=batch_idx + 1,
+                    size=len(train_loader),
+                    data=data_time.val,
+                    bt=batch_time.val,
+                    total=bar.elapsed_td,
+                    eta=bar.eta_td,
+                    loss=losses.avg,
+                    top1=top1.avg,
+                    top5=top5.avg,
+                    )
+        bar.next()
+    bar.finish()
+    return (losses.avg, top1.avg)
+
+
+def test(val_loader, model, criterion, epoch, use_cuda):
+    global best_acc
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+    with torch.no_grad():
+        end = time.time()
+        bar = Bar('Processing', max=len(val_loader))
+        for batch_idx, (inputs, targets) in enumerate(val_loader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
+
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+            losses.update(loss.data.item(), inputs.size(0))
+            top1.update(prec1.item(), inputs.size(0))
+            top5.update(prec5.item(), inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # plot progress
+            bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                        batch=batch_idx + 1,
+                        size=len(val_loader),
+                        data=data_time.avg,
+                        bt=batch_time.avg,
+                        total=bar.elapsed_td,
+                        eta=bar.eta_td,
+                        loss=losses.avg,
+                        top1=top1.avg,
+                        top5=top5.avg,
+                        )
+            bar.next()
+        bar.finish()
+    return (losses.avg, top1.avg)
+
+
+def save_checkpoint(state, is_best, checkpoint='checkpoint', filename='checkpoint.pth.tar'):
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(checkpoint, 'model_best.pth.tar'))
+
+
+def adjust_learning_rate(optimizer, epoch):
+    global state
+    if epoch in args.schedule:
+        state['lr'] *= args.gamma
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = state['lr']
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ bbox_arrange_val.py
@@ -0,0 +1,143 @@
+"""
+Use bbox.csv to re-arrange the ILSVRC2012/val/ into ILSVRC2012/bbox???/val/,
+where ??? is the size of the bbox. So inside each bbox???/val/, we still have 1000 class,
+just the number of val image for each class is different. We will use histogram to determine 10
+different image areas and then split images accordingly.
+
+You have to first run /BS/yfan/nobackup/mkdir.sh to create the structure of the folders for copying images
+"""
+import os
+import numpy as np
+from shutil import copyfile
+from tqdm import tqdm
+
+name2class = dict()  # {'ILSVRC2012_val_00000001.JPEG': 'n01751748/' ... 'ILSVRC2012_val_00050000.JPEG': 'n02437616/'}
+id2name = dict()
+with open('/BS/yfan/nobackup/valprep.sh', 'r') as f:
+    txt = f.readlines()
+k = 0
+for line in txt:
+    if line[1] == 'v':
+        tmp = line[:-2].split(' ')
+        name2class[tmp[1]] = tmp[2]
+        id2name[k] = tmp[1]
+        k += 1
+
+
+def histedges_equalN(x, nbin):
+    npt = len(x)  # 50000
+    return np.interp(np.linspace(0, npt, nbin + 1),
+                     np.arange(npt),
+                     np.sort(x))
+
+
+with open('/BS/yfan/nobackup/xmls/ILSVRC2012_bbox_val.csv', 'r') as f:
+    txt = f.readlines()
+print('total number of lines:', len(txt))
+name2area = dict()
+for i, line in enumerate(txt):
+    tmp = line[:-1].split(',')
+    xmin = float(tmp[1])
+    ymin = float(tmp[2])
+    xmax = float(tmp[3])
+    ymax = float(tmp[4])
+    area = (xmax-xmin)*(ymax-ymin)
+    if tmp[0] in name2area.keys():
+        name2area[tmp[0]].append(area)
+    else:
+        name2area[tmp[0]] = [area]
+
+null_list = []    # get rid of multilabeled images
+mean_list = []    # take the mean of multilabeled images
+median_list = []  # take the median of multilabeled images
+max_list = []     # take the max of multilabeled images
+for name, area in name2area.items():
+    if len(area) == 1:
+        null_list.append(area[0])
+    mean_list.append(np.mean(area))
+    max_list.append(np.max(area))
+    median_list.append(np.median(area))
+
+num_bins = 5
+mean_bins = histedges_equalN(mean_list, num_bins)
+max_bins = histedges_equalN(max_list, num_bins)
+median_bins = histedges_equalN(median_list, num_bins)
+null_bins = histedges_equalN(null_list, num_bins)
+
+# for i in tqdm(range(50000)):
+#     area = mean_list[i]
+#     if area < mean_bins[1]:
+#         src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+#         dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/mean/bbox0', name2class[id2name[i]], id2name[i])
+#     elif area < mean_bins[2] and area >= mean_bins[1]:
+#         src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+#         dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/mean/bbox1', name2class[id2name[i]], id2name[i])
+#     elif area < mean_bins[3] and area >= mean_bins[2]:
+#         src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+#         dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/mean/bbox2', name2class[id2name[i]], id2name[i])
+#     elif area < mean_bins[4] and area >= mean_bins[3]:
+#         src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+#         dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/mean/bbox3', name2class[id2name[i]], id2name[i])
+#     else:
+#         src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+#         dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/mean/bbox4', name2class[id2name[i]], id2name[i])
+#     copyfile(src, dst)
+
+for i in tqdm(range(50000)):
+    area = max_list[i]
+    if area < max_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/max/bbox0', name2class[id2name[i]], id2name[i])
+    elif area < max_bins[2] and area >= max_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/max/bbox1', name2class[id2name[i]], id2name[i])
+    elif area < max_bins[3] and area >= max_bins[2]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/max/bbox2', name2class[id2name[i]], id2name[i])
+    elif area < max_bins[4] and area >= max_bins[3]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/max/bbox3', name2class[id2name[i]], id2name[i])
+    else:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/max/bbox4', name2class[id2name[i]], id2name[i])
+    copyfile(src, dst)
+
+for i in tqdm(range(50000)):
+    area = median_list[i]
+    if area < median_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/median/bbox0', name2class[id2name[i]], id2name[i])
+    elif area < median_bins[2] and area >= median_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/median/bbox1', name2class[id2name[i]], id2name[i])
+    elif area < median_bins[3] and area >= median_bins[2]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/median/bbox2', name2class[id2name[i]], id2name[i])
+    elif area < median_bins[4] and area >= median_bins[3]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/median/bbox3', name2class[id2name[i]], id2name[i])
+    else:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/median/bbox4', name2class[id2name[i]], id2name[i])
+    copyfile(src, dst)
+
+for i in tqdm(range(len(null_list))):
+    area = null_list[i]
+    if area < null_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/null/bbox0', name2class[id2name[i]], id2name[i])
+    elif area < null_bins[2] and area >= null_bins[1]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/null/bbox1', name2class[id2name[i]], id2name[i])
+    elif area < null_bins[3] and area >= null_bins[2]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/null/bbox2', name2class[id2name[i]], id2name[i])
+    elif area < null_bins[4] and area >= null_bins[3]:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/null/bbox3', name2class[id2name[i]], id2name[i])
+    else:
+        src = os.path.join('/BS/xian/work/data/imageNet1K/val/', id2name[i])
+        dst = os.path.join('/BS/yfan/nobackup/ILSVRC2012_bbox_val/null/bbox4', name2class[id2name[i]], id2name[i])
+    copyfile(src, dst)
+
+
